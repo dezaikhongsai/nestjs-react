00e1b64c0bac9037a38a8f02e4ee0d09d6bd413d
dezaikhongsai <dezaikhongsai2004@gmail.com>
2025-08-07 16:51:33 +0700

    update cors mobile expo

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.setGlobalPrefix(API_PREFIX);
   app.useGlobalFilters(new AllExceptionsFilter());
   
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000 , '0.0.0.0');
 
   console.log(`Application is running on: http://localhost:${process.env.PORT ?? 8081}`);
 }
